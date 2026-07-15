05de03358022196d41d7d83191f7e0d5c66a5cc7
Haolader91 <haolader910@gmail.com>
2026-07-16 00:39:30 +0600

    issue solved

--- proxy.ts
+++ proxy.ts
@@ -7,7 +7,6 @@ export async function proxy(request: NextRequest) {
 
   const session = await auth.api.getSession({
     headers: request.headers,
-    baseURL: request.nextUrl.origin,
   });
 
   if (!session) {
